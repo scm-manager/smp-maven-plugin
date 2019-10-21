739235c12d42a6206a6140291e272a19fc98eea5
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-10-21 14:46:30 +0200

    update buildfrontend-maven-plugin to version 2.5.0

--- src/main/java/sonia/scm/maven/ui/TestUIMojo.java
+++ src/main/java/sonia/scm/maven/ui/TestUIMojo.java
@@ -1,13 +1,12 @@
 package sonia.scm.maven.ui;
 
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
 @Mojo(name = "test-ui")
 public class TestUIMojo extends AbstractUIMojo {
   @Override
-  public void execute() throws MojoExecutionException, MojoFailureException {
+  public void execute() throws MojoExecutionException {
     executeScript("test");
   }
 }
