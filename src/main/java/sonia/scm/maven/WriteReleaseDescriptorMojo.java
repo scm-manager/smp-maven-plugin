bb53ea94126f60aa264c3064f8400d8ccd6df7f2
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2020-04-08 15:10:40 +0200

    Add mojo to create plugin center release file

---
+++ src/main/java/sonia/scm/maven/WriteReleaseDescriptorMojo.java
@@ -0,0 +1,131 @@
+package sonia.scm.maven;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.google.common.hash.Hashing;
+import lombok.Getter;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import static com.google.common.io.Files.asByteSource;
+
+@Mojo(
+  name = "write-release-descriptor",
+  requiresDependencyResolution = ResolutionScope.NONE
+)
+public class WriteReleaseDescriptorMojo extends AbstractDescriptorMojo {
+  @Parameter(defaultValue = "${project}", required = true, readonly = true)
+  private MavenProject project;
+
+  @Parameter(property = "releaseDescriptor", required = true, readonly = true)
+  private String releaseDescriptorFile;
+
+  @Override
+  protected void execute(File descriptor) throws MojoFailureException {
+    ReleaseDescriptor releaseDescriptor = createReleaseDesciptor(readPluginFile(descriptor));
+    try {
+      createYamlMapper().writeValue(new File(releaseDescriptorFile), releaseDescriptor);
+    } catch (IOException e) {
+      throw new MojoFailureException("could not write descriptor", e);
+    }
+  }
+
+  private ReleaseDescriptor createReleaseDesciptor(Plugin plugin) throws MojoFailureException {
+    ReleaseDescriptor releaseDescriptor = new ReleaseDescriptor();
+    releaseDescriptor.plugin = project.getArtifactId();
+    releaseDescriptor.tag = project.getVersion();
+    releaseDescriptor.date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(new Date());
+    releaseDescriptor.checksum = computeCheckSum();
+    releaseDescriptor.conditions = plugin.conditions;
+    releaseDescriptor.dependencies.addAll(plugin.dependencies);
+    return releaseDescriptor;
+  }
+
+  private ObjectMapper createYamlMapper() {
+    ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER));
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    return mapper;
+  }
+
+  private Plugin readPluginFile(File descriptor) throws MojoFailureException {
+    try {
+      JAXBContext jaxbContext = JAXBContext.newInstance(Plugin.class);
+
+      Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+      return (Plugin) jaxbUnmarshaller.unmarshal(descriptor);
+    } catch (JAXBException e) {
+      throw new MojoFailureException("could not read plugin.xml", e);
+    }
+  }
+
+  void setProject(MavenProject project) {
+    this.project = project;
+  }
+
+  void setReleaseDescriptorFile(String releaseDescriptorFile) {
+    this.releaseDescriptorFile = releaseDescriptorFile;
+  }
+
+  private String computeCheckSum() throws MojoFailureException {
+    try {
+      return asByteSource(project.getArtifact().getFile()).hash(Hashing.sha256()).toString();
+    } catch (IOException e) {
+      throw new MojoFailureException("could not compute checksum of artifact", e);
+    }
+  }
+
+  @Getter
+  private static class ReleaseDescriptor {
+    String plugin;
+    String tag;
+    String date;
+    String checksum;
+    Collection<String> dependencies = new ArrayList<>();
+    PluginConditions conditions;
+  }
+
+  @XmlRootElement(name = "plugin")
+  @XmlAccessorType(XmlAccessType.FIELD)
+  static class Plugin {
+    @XmlElement(name = "scm-version")
+    private String scmVersion;
+
+    private PluginConditions conditions;
+
+    @XmlElementWrapper(name="dependencies")
+    @XmlElement(name = "dependency")
+    private List<String> dependencies;
+  }
+
+  @XmlRootElement(name = "information")
+  @XmlAccessorType(XmlAccessType.FIELD)
+  @Getter
+  static class PluginConditions {
+    @XmlElement(name = "min-version")
+    private String minVersion;
+    private String os;
+    private String arch;
+  }
+}
---
+++ src/test/java/sonia/scm/maven/WriteReleaseDescriptorMojoTest.java
@@ -0,0 +1,74 @@
+package sonia.scm.maven;
+
+import com.google.common.io.ByteSource;
+import com.google.common.io.Resources;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junitpioneer.jupiter.TempDirectory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static edu.emory.mathcs.backport.java.util.Collections.singleton;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(TempDirectory.class)
+class WriteReleaseDescriptorMojoTest {
+
+  Path pluginFile;
+  Path artifactFile;
+
+  @BeforeEach
+  void writePluginFile(@TempDirectory.TempDir Path temp) throws IOException {
+    pluginFile = temp.resolve("plugin.xml");
+    ByteSource byteSource = Resources.asByteSource(Resources.getResource("plugin.xml"));
+    byteSource.copyTo(Files.newOutputStream(pluginFile));
+  }
+
+  @BeforeEach
+  void writeArtifactFile(@TempDirectory.TempDir Path temp) throws IOException {
+    artifactFile = temp.resolve("scm-review-plugin.smp");
+    Files.write(artifactFile, singleton("this is some plugin file"));
+  }
+
+  @Test
+  void shouldWriteDescriptorFile(@TempDirectory.TempDir Path temp) throws MojoFailureException, IOException {
+    WriteReleaseDescriptorMojo mojo = new WriteReleaseDescriptorMojo();
+    mojo.setDescriptorPath(pluginFile.toString());
+    Path resultFile = temp.resolve("plugin.yaml");
+    mojo.setReleaseDescriptorFile(resultFile.toString());
+    MavenProject project = new MavenProject();
+    project.setArtifactId("scm-review-plugin");
+    project.setVersion("2.1.0");
+    Artifact artifact = mock(Artifact.class);
+    when(artifact.getFile()).thenReturn(artifactFile.toFile());
+    project.setArtifact(artifact);
+    mojo.setProject(project);
+    mojo.execute(pluginFile.toFile());
+
+    List<String> resultContent = Files.readAllLines(resultFile);
+
+    assertThat(resultContent).contains(
+      "plugin: \"scm-review-plugin\"",
+      "tag: \"2.1.0\"",
+      "checksum: \"ccc88166f98b68935a605ed9b595e94cfed4c9047df2e0feacc4776aaadeab4c\"",
+      "dependencies:",
+      "- \"scm-mail-plugin\"",
+      "- \"scm-other-plugin\"",
+      "conditions:",
+      "  minVersion: \"2.2.0\"",
+      "  os: \"Linux\"",
+      "  arch: \"arm\""
+    );
+    assertThat(resultContent.stream())
+      .anyMatch(s -> s.matches("date: \".*\""));
+  }
+}
